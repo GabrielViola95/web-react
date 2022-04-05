9b4c7eaad7d6c8e31060d6a283b6f13d45372df7
GabrielViola95 <92824009+GabrielViola95@users.noreply.github.com>
2022-04-05 00:09:46 -0300

    404 fixed

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,11 +1,17 @@
-import NotFounde from '../../assets/notFound.png'
+import astronauta from '../../assets/astronauta.png';
+import textoPosra from '../../assets/textoPosra.png';
+import Psykonautas from '../../assets/Psykonautas.png';
 
 const NotFound = () => {
     return (
         <div className='erro404'>
-        <div className='notFound'>
-            {/* <img className='notFound__img' src={NotFounde} alt="error404" /> */}
-        </div>
+            <div className='notFound'>
+                <img className='notFound__img textoPosra' src={textoPosra} alt="textoPosra guidano psiconauta" />
+                <div className="cartel-wrap">
+                    <img className='notFound__img cartel' src={Psykonautas} alt="Psykonautas guidano psiconauta" />
+                </div>
+                <img className='notFound__img astronauta' src={astronauta} alt="astronauta guidano psiconauta" />
+            </div>
         </div>
     )
 }
